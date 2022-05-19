a5db77c0fa89b042643af73fa0a7ad5ffdc81862
Grib Cyr <1832331+gcyrillus@users.noreply.github.com>
2022-05-20 00:18:35 +0200

    Add files via upload
    [/] updated to be efficient

--- ComSite/ComSite.php
+++ ComSite/ComSite.php
@@ -1,6 +1,8 @@
 <?php
 class ComSite extends plxPlugin {	 
-
+	const BEGIN_CODE = '<?php' . PHP_EOL;
+	const END_CODE = PHP_EOL . '?>';
+    
 	public function __construct($default_lang) {
 
 		# appel du constructeur de la classe plxPlugin (obligatoire)
@@ -9,11 +11,12 @@ class ComSite extends plxPlugin {
 		# déclaration des hooks
 		$this->addHook('plxShowLastComList', 'plxShowLastComList');
 		
+
 	}	
-	public function plxShowLastComList($format, $max = 5, $art_id = '', $cat_ids = ''){ 
-           if (!$format)  $format='<li><a href="#com_url">#com_author a dit :</a><p>#com_content(50) | Site web : <a href="#com_site">#com_site</a></p></li>';
-    global $plxMotor;
-       # Génération de notre motif
+	public function plxShowLastComList(){ 
+			echo self::BEGIN_CODE;
+?>
+ # Génération de notre motif
         if (empty($art_id))
             $motif = '/^[0-9]{4}.[0-9]{10}-[0-9]+.xml$/';
         else
@@ -22,16 +25,16 @@ class ComSite extends plxPlugin {
         $count = 1;
         $datetime = date('YmdHi');
         # Nouvel objet plxGlob et récupération des fichiers
-        $plxGlob_coms = clone $plxMotor->plxGlob_coms;
+        $plxGlob_coms = clone $this->plxMotor->plxGlob_coms;
         if ($aFiles = $plxGlob_coms->query($motif, 'com', 'rsort', 0, false, 'before')) {
             $aComArtTitles = array(); # tableau contenant les titres des articles
             $isComArtTitle = (strpos($format, '#com_art_title') != FALSE) ? true : false;
             # On parcourt les fichiers des commentaires
             foreach ($aFiles as $v) {
                 # On filtre si le commentaire appartient à un article d'une catégorie inactive
-                if (isset($plxMotor->activeArts[substr($v, 0, 4)])) {
-                    $com = $plxMotor->parseCommentaire(PLX_ROOT . $plxMotor->aConf['racine_commentaires'] . $v);
-                    $artInfo = $plxMotor->artInfoFromFilename($plxMotor->plxGlob_arts->aFiles[$com['article']]);
+                if (isset($this->plxMotor->activeArts[substr($v, 0, 4)])) {
+                    $com = $this->plxMotor->parseCommentaire(PLX_ROOT . $this->plxMotor->aConf['racine_commentaires'] . $v);
+                    $artInfo = $this->plxMotor->artInfoFromFilename($this->plxMotor->plxGlob_arts->aFiles[$com['article']]);
                     if ($artInfo['artDate'] <= $datetime) { # on ne prends que les commentaires pour les articles publiés
                         if (empty($cat_ids) or preg_match('/(' . $cat_ids . ')/', $artInfo['catId'])) {
                             $url = '?article' . intval($com['article']) . '/' . $artInfo['artUrl'] . '#c' . $com['article'] . '-' . $com['index'];
@@ -40,11 +43,9 @@ class ComSite extends plxPlugin {
                             # On modifie nos motifs
                             $row = str_replace('L_SAID', L_SAID, $format);
                             $row = str_replace('#com_id', $com['index'], $row);
-                            $row = str_replace('#com_url', $plxMotor->urlRewrite($url), $row);
+                            $row = str_replace('#com_url', $this->plxMotor->urlRewrite($url), $row);
                             $row = str_replace('#com_author', $com['author'], $row);
-			// ajout du champ pour extraire l'url du site 
                             $row = str_replace('#com_site', $com['site'], $row);
-			// fin ajout
                             while (preg_match('/#com_content\(([0-9]+)\)/', $row, $capture)) {
                                 if ($com['author'] == 'admin')
                                     $row = str_replace('#com_content(' . $capture[1] . ')', plxUtils::strCut($content, $capture[1]), $row);
@@ -60,8 +61,8 @@ class ComSite extends plxPlugin {
                                 if (isset($aComArtTitles[$com['article']])) {
                                     $row = str_replace('#com_art_title', $aComArtTitles[$com['article']], $row);
                                 } else {
-                                    if ($file = $plxMotor->plxGlob_arts->query('/^' . $com['article'] . '.(.*).xml$/')) {
-                                        $art = $plxMotor->parseArticle(PLX_ROOT . $this->plxMotor->aConf['racine_articles'] . $file[0]);
+                                    if ($file = $this->plxMotor->plxGlob_arts->query('/^' . $com['article'] . '.(.*).xml$/')) {
+                                        $art = $this->plxMotor->parseArticle(PLX_ROOT . $this->plxMotor->aConf['racine_articles'] . $file[0]);
                                         $aComArtTitles[$com['article']] = $art_title = $art['title'];
                                         $row = str_replace('#com_art_title', $art_title, $row);
                                     }
@@ -76,7 +77,10 @@ class ComSite extends plxPlugin {
                 if ($count > $max) break;
             }
         }
-      exit; // pour ne pas lancer la suite de la fonction native
+        exit;
+<?php
+		echo self::END_CODE;
+        
        }
 }
 ?>
