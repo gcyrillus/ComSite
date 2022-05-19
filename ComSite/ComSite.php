751fb9e1df19141d50e2188aee71baad3e828c44
Grib Cyr <1832331+gcyrillus@users.noreply.github.com>
2022-05-19 23:43:53 +0200

    Add files via upload

---
+++ ComSite/ComSite.php
@@ -0,0 +1,82 @@
+<?php
+class ComSite extends plxPlugin {	 
+
+	public function __construct($default_lang) {
+
+		# appel du constructeur de la classe plxPlugin (obligatoire)
+		parent::__construct($default_lang);
+		
+		# déclaration des hooks
+		$this->addHook('plxShowLastComList', 'plxShowLastComList');
+		
+	}	
+	public function plxShowLastComList($format, $max = 5, $art_id = '', $cat_ids = ''){ 
+           if (!$format)  $format='<li><a href="#com_url">#com_author a dit :</a><p>#com_content(50) | Site web : <a href="#com_site">#com_site</a></p></li>';
+    global $plxMotor;
+       # Génération de notre motif
+        if (empty($art_id))
+            $motif = '/^[0-9]{4}.[0-9]{10}-[0-9]+.xml$/';
+        else
+            $motif = '/^' . str_pad($art_id, 4, '0', STR_PAD_LEFT) . '.[0-9]{10}-[0-9]+.xml$/';
+
+        $count = 1;
+        $datetime = date('YmdHi');
+        # Nouvel objet plxGlob et récupération des fichiers
+        $plxGlob_coms = clone $plxMotor->plxGlob_coms;
+        if ($aFiles = $plxGlob_coms->query($motif, 'com', 'rsort', 0, false, 'before')) {
+            $aComArtTitles = array(); # tableau contenant les titres des articles
+            $isComArtTitle = (strpos($format, '#com_art_title') != FALSE) ? true : false;
+            # On parcourt les fichiers des commentaires
+            foreach ($aFiles as $v) {
+                # On filtre si le commentaire appartient à un article d'une catégorie inactive
+                if (isset($plxMotor->activeArts[substr($v, 0, 4)])) {
+                    $com = $plxMotor->parseCommentaire(PLX_ROOT . $plxMotor->aConf['racine_commentaires'] . $v);
+                    $artInfo = $plxMotor->artInfoFromFilename($plxMotor->plxGlob_arts->aFiles[$com['article']]);
+                    if ($artInfo['artDate'] <= $datetime) { # on ne prends que les commentaires pour les articles publiés
+                        if (empty($cat_ids) or preg_match('/(' . $cat_ids . ')/', $artInfo['catId'])) {
+                            $url = '?article' . intval($com['article']) . '/' . $artInfo['artUrl'] . '#c' . $com['article'] . '-' . $com['index'];
+                            $date = $com['date'];
+                            $content = strip_tags($com['content']);
+                            # On modifie nos motifs
+                            $row = str_replace('L_SAID', L_SAID, $format);
+                            $row = str_replace('#com_id', $com['index'], $row);
+                            $row = str_replace('#com_url', $plxMotor->urlRewrite($url), $row);
+                            $row = str_replace('#com_author', $com['author'], $row);
+			// ajout du champ pour extraire l'url du site 
+                            $row = str_replace('#com_site', $com['site'], $row);
+			// fin ajout
+                            while (preg_match('/#com_content\(([0-9]+)\)/', $row, $capture)) {
+                                if ($com['author'] == 'admin')
+                                    $row = str_replace('#com_content(' . $capture[1] . ')', plxUtils::strCut($content, $capture[1]), $row);
+                                else
+                                    $row = str_replace('#com_content(' . $capture[1] . ')', plxUtils::strCheck(plxUtils::strCut(plxUtils::strRevCheck($content), $capture[1])), $row);
+                            }
+                            $row = str_replace('#com_content', $content, $row);
+                            $row = str_replace('#com_date', plxDate::formatDate($date, '#num_day/#num_month/#num_year(4)'), $row);
+                            $row = str_replace('#com_hour', plxDate::formatDate($date, '#time'), $row);
+                            $row = plxDate::formatDate($date, $row);
+                            # récupération du titre de l'article
+                            if ($isComArtTitle) {
+                                if (isset($aComArtTitles[$com['article']])) {
+                                    $row = str_replace('#com_art_title', $aComArtTitles[$com['article']], $row);
+                                } else {
+                                    if ($file = $plxMotor->plxGlob_arts->query('/^' . $com['article'] . '.(.*).xml$/')) {
+                                        $art = $plxMotor->parseArticle(PLX_ROOT . $this->plxMotor->aConf['racine_articles'] . $file[0]);
+                                        $aComArtTitles[$com['article']] = $art_title = $art['title'];
+                                        $row = str_replace('#com_art_title', $art_title, $row);
+                                    }
+                                }
+                            }
+                            # On genère notre ligne
+                            echo $row;
+                            $count++;
+                        }
+                    }
+                }
+                if ($count > $max) break;
+            }
+        }
+      exit; // pour ne pas lancer la suite de la fonction native
+       }
+}
+?>
