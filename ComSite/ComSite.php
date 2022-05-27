2b159564aaf291c6ce60dd84b4a5db7bb458e81e
Grib Cyr <1832331+gcyrillus@users.noreply.github.com>
2022-05-27 07:14:01 +0200

    Add files via upload

--- ComSite/ComSite.php
+++ ComSite/ComSite.php
@@ -45,7 +45,8 @@ class ComSite extends plxPlugin {
                             $row = str_replace('#com_id', $com['index'], $row);
                             $row = str_replace('#com_url', $this->plxMotor->urlRewrite($url), $row);
                             $row = str_replace('#com_author', $com['author'], $row);
-                            $row = str_replace('#com_site', $com['site'], $row);
+							if($com['site']=='') {$row = str_replace('#com_site', $com['site'].'" style="display:none;"', $row);}
+                            else {$row = str_replace('#com_site', $com['site'], $row);}
                             while (preg_match('/#com_content\(([0-9]+)\)/', $row, $capture)) {
                                 if ($com['author'] == 'admin')
                                     $row = str_replace('#com_content(' . $capture[1] . ')', plxUtils::strCut($content, $capture[1]), $row);
@@ -77,8 +78,7 @@ class ComSite extends plxPlugin {
                 if ($count > $max) break;
             }
         }
-        //exit; 
-        $format=''; // stoppe l'affichage
+        $format="";
 <?php
 		echo self::END_CODE;
         
